081e3e322dfb755f98505bc87344d2696a0a5921
jennings <jennings.treutel@centralsquare.com>
2020-09-23 09:29:45 -0500

    Added some comments.

--- AWS/xacct_ecr_image_copy.ps1
+++ AWS/xacct_ecr_image_copy.ps1
@@ -1,25 +1,34 @@
-$sourceAcctProfile = "source_account_aws_profile"
-$sourceAcctRegion = "us-west-1"
-$destAcctProfile = "dest_account_aws_profile"
-$destAcctRegion = "us-west-1"
-$targetImages = "foo","bar","baz"
-$imageTag = "SOMETAG"
+#You should run this on from a machine with a fast connection and enough space to temporarily host all of the images locally.
+#Running this from a temporary EC2 instance is recommended.
+
+$sourceAcctProfile 	= "some_aws_profile_name"
+$sourceAcctRegion 	= "us-east-1"
+
+$destAcctProfile    = "some_other_aws_profile_name"
+$destAcctRegion 	  = "us-west-1"
+
+$targetImages 		  = "foo","bar","baz"
+$imageTag 			    = "SOMETAG"
+
 $sourceAcctNo = (aws sts get-caller-identity --profile $sourceAcctProfile --region $sourceAcctRegion | ConvertFrom-Json).Account
 $destAcctNo = (aws sts get-caller-identity --profile $destAcctProfile --region $destAcctRegion | ConvertFrom-Json).Account
 
-# Pull images from source ECR
+
+# Pull down Docker images locally from source ECR repos
 aws ecr get-login-password --region $sourceAcctRegion --profile $sourceAcctProfile | docker login --username AWS --password-stdin https://$sourceAcctNo.dkr.ecr.$sourceAcctRegion.amazonaws.com
 foreach($image in $targetImages) {
   docker pull "$sourceAcctNo.dkr.ecr.$sourceAcctRegion.amazonaws.com/${image}:${imageTag}"
 }
 docker logout https://$sourceAcctNo.dkr.ecr.$sourceAcctRegion.amazonaws.com
 
-# Retag images locally
+
+# Retag Docker images locally
 foreach($image in $targetImages) {
   docker tag "$sourceAcctNo.dkr.ecr.$sourceAcctRegion.amazonaws.com/${image}:${imageTag}" "$destAcctNo.dkr.ecr.$destAcctRegion.amazonaws.com/${image}:${imageTag}"
 }
 
-# Push images to target ECR
+
+# Push retagged Docker images  to destination repo
 aws ecr get-login-password --region $destAcctRegion --profile $destAcctProfile | docker login --username AWS --password-stdin https://$destAcctNo.dkr.ecr.$destAcctRegion.amazonaws.com
 foreach($image in $targetImages) {
   docker push "$destAcctNo.dkr.ecr.$destAcctRegion.amazonaws.com/${image}:${imageTag}"
--- AWS/xacct_ecr_image_copy.sh
+++ AWS/xacct_ecr_image_copy.sh
@@ -1,25 +1,31 @@
-SOURCE_ACCT_PROFILE="source_account_aws_profile"
-SOURCE_ACCT_REGION="us-west-1"
-DEST_ACCT_PROFILE="dest_account_aws_profile"
+#You should run this on from a machine with a fast connection and enough space to temporarily host all of the images locally.
+#Running this from a temporary EC2 instance is recommended.
+
+SOURCE_ACCT_PROFILE="some_aws_profile_name"
+SOURCE_ACCT_REGION="us-east-1"
+
+DEST_ACCT_PROFILE="some_other_aws_profile_name"
 DEST_ACCT_REGION="us-west-1"
+
 TARGET_IMAGES=("foo" "bar" "baz")
 IMAGE_TAG="SOMETAG"
+
 SOURCE_ACCT_NO=$(aws sts get-caller-identity --profile $SOURCE_ACCT_PROFILE --region $SOURCE_ACCT_REGION | jq -r '.Account')
 DEST_ACCT_NO=$(aws sts get-caller-identity --profile $DEST_ACCT_PROFILE --region $DEST_ACCT_REGION | jq -r '.Account')
 
-# Pull images from source ECR
+# Pull down Docker images locally from source ECR repos
 docker login -u AWS -p $(aws ecr get-login-password --region $SOURCE_ACCT_REGION --profile $SOURCE_ACCT_PROFILE) $SOURCE_ACCT_NO.dkr.ecr.$SOURCE_ACCT_REGION.amazonaws.com
 for IMAGE in ${TARGET_IMAGES[@]}; do
   docker pull "$SOURCE_ACCT_NO.dkr.ecr.$SOURCE_ACCT_REGION.amazonaws.com/$IMAGE:$IMAGE_TAG"
 done
-
-# Retag images locally
 docker logout $SOURCE_ACCT_NO.dkr.ecr.$SOURCE_ACCT_REGION.amazonaws.com
+
+# Retag Docker images locally
 for IMAGE in ${TARGET_IMAGES[@]}; do
   docker tag "$SOURCE_ACCT_NO.dkr.ecr.$SOURCE_ACCT_REGION.amazonaws.com/$IMAGE:$IMAGE_TAG" "$DEST_ACCT_NO.dkr.ecr.$DEST_ACCT_REGION.amazonaws.com/$IMAGE:$IMAGE_TAG"
 done
 
-# Push images to target ECR
+# Push retagged Docker images  to destination repo
 docker login -u AWS -p $(aws ecr get-login-password --region $DEST_ACCT_REGION --profile $DEST_ACCT_PROFILE) $DEST_ACCT_NO.dkr.ecr.$DEST_ACCT_REGION.amazonaws.com
 for IMAGE in ${TARGET_IMAGES[@]}; do
   docker push "$DEST_ACCT_NO.dkr.ecr.$DEST_ACCT_REGION.amazonaws.com/$IMAGE:$IMAGE_TAG"
