e3a6dabd6eca0bf9b17bfdfc4a2cd66c28343397
Jennings <jtreutel+github@protonmail.com>
2021-12-03 16:56:11 +0900

    Forgot wget command.

--- linux/bash_one_liners.sh
+++ linux/bash_one_liners.sh
@@ -2,4 +2,4 @@
 openssl s_client -showcerts -verify 32 -connect sts.amazonaws.com:443
 
 #Get the latest CircleCI CLI tarball for Linux
-curl https://api.github.com/repos/CircleCI-Public/circleci-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux_amd64")) | .browser_download_url'
+curl https://api.github.com/repos/CircleCI-Public/circleci-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux_amd64")) | .browser_download_url' | wget -qi -
