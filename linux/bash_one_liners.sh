1e73be689a8f57daa5599de544868b4132483cdb
Jennings <jtreutel+github@protonmail.com>
2021-12-03 16:54:19 +0900

    CircleCI CLI jq command.

--- linux/bash_one_liners.sh
+++ linux/bash_one_liners.sh
@@ -1,3 +1,5 @@
 #Check cert chain for MITM
 openssl s_client -showcerts -verify 32 -connect sts.amazonaws.com:443
 
+#Get the latest CircleCI CLI tarball for Linux
+curl https://api.github.com/repos/CircleCI-Public/circleci-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux_amd64")) | .browser_download_url'
